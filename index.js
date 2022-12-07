d959667342bef1ad0f77771c054fd214d5a91366
baraza7 <barazabrian87@gmail.com>
2022-12-07 16:04:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here!
+main.remove();
+
+// const newHeader = document.querySelectorAll('h1 victory');
+
+// 1) index.html
+//        after index.js is processed
+//          has a 'newHeader' variable that points to node 'h1#victory':
+//      AssertionError: Make sure you create an <h1> with id 'victory': expected undefined to deeply equal 'H1'
+
+// createNewHeder("")
+
+// it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
+//     expect(newHeader.nodeName, "Make sure you create an <h1> with id 'victory'").eql('H1')
+//   });
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+newHeader.textContent = "Brian is the champion"
+
+document.body.append(newHeader);
